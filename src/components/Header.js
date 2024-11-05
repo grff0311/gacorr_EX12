954e339bce5793a954051d55b3847204ff277450
Griffin Roleh <griffinroleh24113@gmail.com>
2024-11-05 07:12:04 +0700

    griffin

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from './Logo';
+import Generations from './Generations';
+
+function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+      <Logo />
+      <Generations />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Search from './Search';
+import CardInfo from './CardInfo';
+import Logo from './Logo';
+
+function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <Search />
+      <CardInfo />
+      <Logo />
+    </main>
+  );
+}
+
+export default Home;

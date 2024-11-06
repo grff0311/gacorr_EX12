47d1bdf6b3ec2f1a5e73340d3c4fc8a3d4c8dbaf
frankyarch <angkin00789@gmail.com>
2024-11-06 23:05:01 +0800

    franklyn logo dan cardinfo

---
+++ src/components/Cardinfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Evolution from './Evolution';
+import TypeEffect from './TypeEffect';
+
+function CardInfo() {
+  return (
+    <div>
+      <h1>CardInfo</h1>
+      <Evolution />
+      <TypeEffect />
+    </div>
+  );
+}
+
+export default CardInfo;
